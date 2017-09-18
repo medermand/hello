18600178e870bf8361deb7091c28947f0c494ba5
medermand <ku.meder@gmail.com>
2017-09-18 17:54:15 +0600

    First commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:3306;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
